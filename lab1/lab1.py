6e39e5e64787a127b253d87f51cf3567abc38885
yelizarovaliza <yyelizarova@kse.org.ua>
2024-06-02 14:48:29 +0300

    shear and scale added

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -19,6 +19,7 @@ def plot_shape(shape, title="Shape"):
 plot_shape(triangle, "Initial Triangle")
 plot_shape(polygon, "Initial Polygon")
 
+#rotation
 def rotate(shape, angle):
     radians = np.deg2rad(angle)
     rotation_matrix = np.array([
@@ -31,4 +32,56 @@ rotated_triangle = rotate(triangle, 45)
 plot_shape(rotated_triangle, "Rotated Triangle by 45 degrees")
 rotated_poygon = rotate(polygon, 30)
 plot_shape(rotated_poygon, "Rotated Polygon by 30 degrees")
+
+# scaling
+def scale(shape, scale_factor):
+    scaling_matrix = np.array([
+        [scale_factor, 0],
+        [0, scale_factor]
+    ])
+    return shape @ scaling_matrix.T
+
+scaled_triangle = scale(triangle, 2)
+plot_shape(scaled_triangle, "Scaled Triangle by factor 2")
+scaled_polygon = scale(polygon, 0.5)
+plot_shape(scaled_polygon, "Scaled Polygon by factor 0.5")
+
+# reflection
+def reflect(shape, axis):
+    if axis == 'x':
+        reflection_matrix = np.array([
+            [1, 0],
+            [0, -1]
+        ])
+    elif axis == 'y':
+        reflection_matrix = np.array([
+            [-1, 0],
+            [0, 1]
+        ])
+    return shape @ reflection_matrix.T
+
+reflected_triangle = reflect(triangle, 'x')
+plot_shape(reflected_triangle, "Reflected Triangle over X axis")
+reflected_polygon = reflect(polygon, 'y')
+plot_shape(reflected_polygon, "Reflected Polygon over Y axis")
+
+# shear
+def shear(shape, axis, shear_factor):
+    if axis == 'x':
+        shear_matrix = np.array([
+            [1, shear_factor],
+            [0, 1]
+        ])
+    elif axis == 'y':
+        shear_matrix = np.array([
+            [1, 0],
+            [shear_factor, 1]
+        ])
+    return shape @ shear_matrix.T
+
+sheared_triangle = shear(triangle, 'x', 1)
+plot_shape(sheared_triangle, "Sheared Triangle along X axis with factor 1")
+sheared_polygon = shear(polygon, 'y', 3)
+plot_shape(sheared_polygon, "Sheared Polygon along Y axis with factor 3")
+
 
